6fa88012c8bc6ea66b3724fef20ced9f4c7c68d8
Ned Batchelder <ned@nedbatchelder.com>
2026-02-01 15:24:51 -0500

    more

--- samples/test_six_divided.py
+++ samples/test_six_divided.py
@@ -4,13 +4,14 @@ import pytest
 def six_divided(x):
     return 6 / x
 
+# Check the happy paths
+
 @pytest.mark.parametrize(
     "x, expected",
     [
         (4, 1.5),
         (3, 2.0),
         (2, 3.0),
-        (1, 6.0),
     ]
 )
 def test_six_divided(x, expected):
@@ -23,7 +24,8 @@ def test_six_divided_by_zero():
         six_divided(0)
 # end
 
-# section3
+# Check the error paths
+
 @pytest.mark.parametrize(
     "x, expected_error",
     [
@@ -35,3 +37,21 @@ def test_six_divided_errors(x, expected_error):
     with pytest.raises(expected_error):
         six_divided(x)
 # end
+
+
+from contextlib import nullcontext as produces
+from pytest import raises
+
+@pytest.mark.parametrize(
+    "x, result",
+    [
+        (4, produces(1.5)),
+        (3, produces(2.0)),
+        (2, produces(3.0)),
+        (0, raises(ZeroDivisionError)),
+        ("cat", raises(TypeError)),
+    ],
+)
+def test_division(x, result):
+    with result as expected:
+        assert six_divided(x) == expected
