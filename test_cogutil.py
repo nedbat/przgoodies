4794a212574cbfd699e4cf83cbbf0716b8a2120a
Ned Batchelder <ned@nedbatchelder.com>
2026-02-03 07:53:35 -0500

    adapt to new string-like EdText

--- test_cogutil.py
+++ test_cogutil.py
@@ -3,15 +3,15 @@ from cogutil import include_file, run_command
 def test_file():
     me = __file__.rstrip("c")
     with open(me) as f:
-        expected = f.readlines()
-    assert include_file(me).lines == expected
+        expected = f.read()
+    assert include_file(me) == expected
 
 
 def test_cmd():
     tp = run_command("seq 3")
-    assert tp.lines == ["$ seq 3\n", "1\n", "2\n", "3\n"]
+    assert tp == "$ seq 3\n1\n2\n3\n"
 
 
 def test_bad_cmd():
     tp = run_command("exit 17")
-    assert tp.lines == ["$ exit 17\n", "(exit code: 17)\n"]
+    assert tp == "$ exit 17\n(exit code: 17)\n"
